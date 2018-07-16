191dfb0d3d509520bc3e97c4be128d92fad45e87
mattwecht <mattwecht@gmail.com>
2018-07-16 00:48:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ class App < Sinatra::Base
   get '/' do 
     "Welcome to your app!!!! 
      This is new\n
-     I made it all myself"
+     I made it all myself
+     And now it is even better"
   end
 
 end
